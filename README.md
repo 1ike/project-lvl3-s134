5b813c059233fb0acadb01c49e6dac2967b41d05
1ike <googlohren@gmail.com>
2017-12-04 20:51:41 +0300

    Fix tests.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/11369a6982008fc1906e/maintainability)](https://codeclimate.com/github/1ike/project-lvl3-s134/maintainability)
 [![Build Status](https://travis-ci.org/1ike/project-lvl3-s134.svg?branch=master)](https://travis-ci.org/1ike/project-lvl3-s134)
 
+Save web page for offline using (like "Save as" in browsers).
+
 ## Installing
 
 Using GIT:
@@ -14,7 +16,7 @@ $ npm link                      # creates global link
 $ cd ~/projects/my-other-app    # go into some other package directory.
 $ npm link page-loader          # link-install the package
 ```
-[see more](https://docs.npmjs.com/cli/link)
+[see more info](https://docs.npmjs.com/cli/link)
 
 ## Use
 
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -18,31 +18,31 @@ const testFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'test-'));
 const pathToFile = path.join(testFolder, testFileName);
 const defaultPathToFile = path.join(process.cwd(), testFileName);
 
-afterEach(() => {
-  fs.exists(pathToFile)
-    .then((exist) => {
-      if (exist) fs.unlink(pathToFile);
-    })
-    .then(() => fs.exists(defaultPathToFile))
-    .then((exist) => {
-      if (exist) fs.unlink(defaultPathToFile);
-    });
-});
+beforeEach(() => fs.readFile(path.join(dir, fixture), 'utf8')
+  .then((html) => {
+    nock(inputURL)
+      .get('/')
+      .reply(200, html);
+  }));
+
+afterEach(() => fs.exists(pathToFile)
+  .then((exist) => {
+    if (exist) fs.unlink(pathToFile);
+  })
+  .then(() => fs.exists(defaultPathToFile))
+  .then((exist) => {
+    if (exist) fs.unlink(defaultPathToFile);
+  }));
 
 test('savePage: saving', () => {
   expect.assertions(1);
 
   return savePage(inputURL, testFolder)
     .then(() => fs.readFile(path.join(dir, fixture), 'utf8'))
-    .then((html) => {
-      nock(inputURL)
-        .get('/')
-        .reply(200, html);
-      return fs.readFile(pathToFile, 'utf8')
-        .then((data) => {
-          expect(data).toBe(html);
-        });
-    });
+    .then(html => fs.readFile(pathToFile, 'utf8')
+      .then((data) => {
+        expect(data).toBe(html);
+      }));
 });
 
 test('savePage: saving default', () => {
@@ -50,15 +50,10 @@ test('savePage: saving default', () => {
 
   return savePage(inputURL)
     .then(() => fs.readFile(path.join(dir, fixture), 'utf8'))
-    .then((html) => {
-      nock(inputURL)
-        .get('/')
-        .reply(200, html);
-      return fs.readFile(defaultPathToFile, 'utf8')
-        .then((data) => {
-          expect(data).toBe(html);
-        });
-    });
+    .then(html => fs.readFile(defaultPathToFile, 'utf8')
+      .then((data) => {
+        expect(data).toBe(html);
+      }));
 });
 
 test('savePage: wrong path', () => {
@@ -68,13 +63,8 @@ test('savePage: wrong path', () => {
 
   return savePage(inputURL, outputPath)
     .then(() => fs.readFile(path.join(dir, fixture), 'utf8'))
-    .then((html) => {
-      nock(inputURL)
-        .get('/')
-        .reply(200, html);
-      return fs.readFile(path.join(outputPath, testFileName), 'utf8')
-        .catch((e) => {
-          expect(e.code).toBe('ENOENT');
-        });
-    });
+    .then(html => fs.readFile(path.join(outputPath, testFileName), 'utf8')
+      .catch((e) => {
+        expect(e.code).toBe('ENOENT');
+      }));
 });
